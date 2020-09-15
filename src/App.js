9a4a86722f1f33511af1769ba838acc1f591b55c
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-09-15 13:32:33 -0400

    Test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
 
-const App = () => <div>Hello World!</div>;
+const App = () => <div>
+  <Route path="/" exact={true} component={HomePage} />
+  <Route path="/login" exact={true} component={LoginPage} />
+</div>;
 
 
 
