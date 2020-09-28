ba6c5880079ad4d9f97f5a0b24436038013808b2
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-09-28 19:46:55 -0400

    User Login-Logout complited

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../../actions/auth';
 
-const HomePage = () => <div>
- <h1>Home Page</h1>
- <Link to="/login">Login</Link>
-</div>;
+const HomePage = ({ isAuthenticated, logout }) => ( 
+<div>
+    <h1>Home Page</h1>
+    {isAuthenticated ? <button onClick={() => logout()}>Logout</button> : <Link to="/login">Login</Link>}
+</div>
+);
 
+HomePage.propTypes = {
+    isAuthenticated: PropTypes.bool.isRequired,
+    logout: PropTypes.func.isRequired
+};
 
+function mapStateToProps(state) {
+    return {
+        isAuthenticated: !!state.user.token
+    }
+}
 
-export default HomePage;
+export default connect(mapStateToProps, { logout: actions.logout })(HomePage);
