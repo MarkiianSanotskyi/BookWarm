56f47f12d568a9ffb6e7c2655033767d3ba2dc40
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-10-08 20:30:26 -0400

    Added function of SignUp, Validation and email Unique

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import { Route } from 'react-router-dom';
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
 import DashboardPage from "./components/pages/DashboardPage";
+import SignupPage from "./components/pages/SignupPage";
 import UserRoute from './components/pages/routes/UserRoute';
 import GuestRoute from "./components/pages/routes/GuestRoute";
+
 
 const App = ({ location }) => (
   <div className="ui container">
     <Route location={location} path="/" exact component={HomePage} />
-    <GuestRoute location={location} path="/login" exact component={props => <LoginPage {...props} />}  />
+    <GuestRoute location={location} path="/login" exact component={LoginPage}  />
+    <GuestRoute
+      location={location}
+      path="/signup"
+      exact
+      component={SignupPage}
+    />
     <UserRoute
       location={location}
       path="/dashboard"
